034ae77cf22da15e0b8ee371615a5c6795d22d64
Vasanth <vasanthprabhakaran1999@gmail.com>
2021-05-26 22:18:21 +0530

    Move to top updated

--- javascript/main.js
+++ javascript/main.js
@@ -1,122 +1,139 @@
 // nav menu
-const showMenu=(toggleId,navId)=>{
-    const toggle=document.getElementById(toggleId)
-    nav=document.getElementById(navId)
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    nav = document.getElementById(navId);
 
-    if(toggle&&nav){
-        toggle.addEventListener('click',()=>{
-            nav.classList.toggle('show')
-        })
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        });
     }
-}
-showMenu('nav-toggle','nav-menu')
+};
+showMenu('nav-toggle', 'nav-menu');
 
 //active and remove
 
-const navLink=document.querySelectorAll('.nav_link');
-function linkAction(){
-    navLink.forEach(n=> n.classList.remove('active'))
-    this.classList.add('active')
+const navLink = document.querySelectorAll('.nav_link');
+function linkAction() {
+    navLink.forEach((n) => n.classList.remove('active'));
+    this.classList.add('active');
 
-    const navMenu=document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
 }
-navLink.forEach(n=>n.addEventListener('click',linkAction));
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
-
-window.addEventListener('scroll',event =>{
-    let navi=document.querySelector('nav');
-    (window.scrollY >= 100)?navi.classList.add('scroll'):navi.classList.remove('scroll');
+window.addEventListener('scroll', (event) => {
+    let navi = document.querySelector('nav');
+    window.scrollY >= 100
+        ? navi.classList.add('scroll')
+        : navi.classList.remove('scroll');
 });
 
+window.addEventListener('scroll', (event) => {
+    let navlinkk = document.querySelectorAll('nav ul li a');
+    let fromTop = window.scrollY;
 
-
-
-window.addEventListener('scroll',event =>{
-    let navlinkk=document.querySelectorAll('nav ul li a');
-    let fromTop=window.scrollY;
-
-    navlinkk.forEach(navik=>{
-        let sec=document.querySelector(navik.hash);
-        if(
-            sec.offsetTop <= fromTop && 
-            sec.offsetTop + sec.offsetHeight> fromTop
-            
+    navlinkk.forEach((navik) => {
+        let sec = document.querySelector(navik.hash);
+        if (
+            sec.offsetTop <= fromTop &&
+            sec.offsetTop + sec.offsetHeight > fromTop
         ) {
             navik.classList.add('active');
-        }
-        else{
+        } else {
             navik.classList.remove('active');
         }
     });
-
 });
-
 
 // ScrollReveal
 
-const srl=ScrollReveal({
-    origin:'bottom',
-    distance:'80px',
-    duration:2000,
-    reset:true
-})
-
+const srl = ScrollReveal({
+    origin: 'bottom',
+    distance: '80px',
+    duration: 2000,
+    reset: true,
+});
 
 //ScrollHOME
-srl.reveal('.home_title',{})
-srl.reveal('.home_button',{delay:100})
-srl.reveal('.home_img',{delay:400})
-srl.reveal('.home_social-icon',{interval:200})
+srl.reveal('.home_title', {});
+srl.reveal('.home_button', { delay: 100 });
+srl.reveal('.home_img', { delay: 400 });
+srl.reveal('.home_social-icon', { interval: 200 });
 
 //ScrollABOUT
-srl.reveal('.about_img',{})
-srl.reveal('.about_subtitle',{})
-srl.reveal('.about_text',{delay:200})
-srl.reveal('.resume_button',{delay:200})
+srl.reveal('.about_img', {});
+srl.reveal('.about_subtitle', {});
+srl.reveal('.about_text', { delay: 200 });
+srl.reveal('.resume_button', { delay: 200 });
 
 //ScrollSKILLS
-srl.reveal('.skills_subtitle',{delay:200})
-srl.reveal('.skills_img',{delay:700})
-srl.reveal('.skills_text',{delay:200})
-srl.reveal('.skills_data',{interval:200})
+srl.reveal('.skills_subtitle', { delay: 200 });
+srl.reveal('.skills_img', { delay: 700 });
+srl.reveal('.skills_text', { delay: 200 });
+srl.reveal('.skills_data', { interval: 200 });
 
 //ScrollWORK
-srl.reveal('.work_img',{interval:200})
+srl.reveal('.work_img', { interval: 200 });
 
 //ScrollContact
-srl.reveal('.contact_list',{interval:200})
-
-
+srl.reveal('.contact_list', { interval: 200 });
 
 //StyleSwitcher
-const links=document.querySelectorAll(".alternate-color"),
-    totalLinks=links.length;
-function setActiveStyle(color){
-    for(let i=0;i<totalLinks;i++){
-        if(color===links[i].getAttribute("title")){
-            links[i].removeAttribute("disabled");
-        }
-        else{
-            links[i].setAttribute("disabled","true");
+const links = document.querySelectorAll('.alternate-color'),
+    totalLinks = links.length;
+function setActiveStyle(color) {
+    for (let i = 0; i < totalLinks; i++) {
+        if (color === links[i].getAttribute('title')) {
+            links[i].removeAttribute('disabled');
+        } else {
+            links[i].setAttribute('disabled', 'true');
         }
     }
 }
-// bodyskin
-const bodySkin=document.querySelectorAll(".body-skin"),
-    totalBodySkin=bodySkin.length;
-    for(let i=0;i<totalBodySkin;i++){
-        bodySkin[i].addEventListener("change",function(){
-            // console.log(this.value)
-            if(this.value==="dark"){
-                document.body.className="dark"
-            }
-            else{
-                document.body.className=""
-            }
-        })
-    }
 
-document.querySelector(".toggle-style_switcher").addEventListener("click",()=>{
-    document.querySelector(".style-switcher").classList.toggle("open");
-})
+document
+    .querySelector('.toggle-style_switcher')
+    .addEventListener('click', () => {
+        document.querySelector('.style-switcher').classList.toggle('open');
+    });
+
+// bodyskin
+const bodySkin = document.querySelectorAll('.body-skin'),
+    totalBodySkin = bodySkin.length;
+for (let i = 0; i < totalBodySkin; i++) {
+    bodySkin[i].addEventListener('change', function () {
+        // console.log(this.value)
+        if (this.value === 'dark') {
+            document.body.className = 'dark';
+        } else {
+            document.body.className = '';
+        }
+    });
+}
+
+// scrollTop
+var mybutton = document.getElementById('myBtn');
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+    ) {
+        mybutton.style.display = 'block';
+    } else {
+        mybutton.style.display = 'none';
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
